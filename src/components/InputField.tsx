75cb1fa6ac88d26d146b2cc1292443bd79a3c4c9
Pavel Bairov <bairovpaul@gmail.com>
2025-06-03 15:33:20 +0700

    Upgrade input

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './styles.css'
 
-export const InputField = () => {
-  return <form className='input'>
-    <input type='input' placeholder='Добавьте новую задачу' className='input-box'/>
+interface Props{
+  todo: string
+  setTodo: React.Dispatch<React.SetStateAction<string>>
+  handleAdd: (e: React.FormEvent) => void
+}
+
+export const InputField = ({ todo, setTodo, handleAdd }: Props) => {
+  return <form className='input' onSubmit={handleAdd}>
+    <input type='input'
+      value={todo}
+      onChange={
+        (e) => setTodo(e.target.value)
+      } 
+      placeholder='Добавьте новую задачу' 
+      className='input-box'/>
     <button className='input-submit' type='submit'>Добавить</button>
   </form>
 }
