dcbc3a6d624ab5a5c13fcef94be7e500f1ac5a53
Pavel Bairov <bairovpaul@gmail.com>
2025-06-03 16:44:08 +0700

    TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './App.css'
 import { InputField } from './components/InputField.tsx'
 import type { Todo } from './model.ts'
+import TodoList from './components/TodoList.tsx'
 
 const App: React.FC = () =>  {
   const [todo, setTodo] = useState<string>('')
@@ -15,10 +16,11 @@ const App: React.FC = () =>  {
       setTodo('')
     }
   };
-  console.log(todos)
+  
   return  <div className='App'>
     <span className='heading'>TodoList</span>
     <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+    <TodoList todos={todos} setTodos={setTodos}/>
   </div>
 }
 
---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import type { Todo } from '../model'
+import { AiFillDelete, AiFillEdit } from 'react-icons/ai'
+import { MdDone } from 'react-icons/md'
+import './styles.css'
+import TodoList from './TodoList'
+
+interface Props{
+  todo: Todo
+  todos: Todo[]
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+}
+
+const SingleTodo = ({ todo, todos, setTodos }: Props)  => {
+
+  const handleDone = (id:number) => {setTodos(todos.map((todo)=>todo.id===id ? {...todo, isDone: !todo.isDone} : todo))}
+  return <form className='todos-single'>
+      {
+        todo.isDone ? (
+          <s className='todos-single-text'>{todo.todo}</s>
+        ) : (
+          <span className='todos-single-text'>{todo.todo}</span>
+        )
+      }
+    <div>
+      <span className='icon'><AiFillEdit/></span>
+      <span className='icon'><AiFillDelete/></span>
+      <span className='icon' onClick={() => handleDone(todo.id)}><MdDone/></span>
+    </div>
+  </form>
+}
+
+export default SingleTodo
